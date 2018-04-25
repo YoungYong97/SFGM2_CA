46d21babf8e36588e06101cfced71c54269a6694
Justine Quiapos <C16402272@mydit.ie>
2018-04-25 15:30:15 +0100

    Just fixed up the layout of the django layout

--- tourGalwayProject/wsgi.py
+++ tourGalwayProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tourgalway project.
+WSGI config for tourGalway project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tourgalway.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tourGalway.settings")
 
 application = get_wsgi_application()
